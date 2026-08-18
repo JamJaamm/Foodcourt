42e86d795d6bc1b08cc1ab20d66dd9e74e7a0366
JamJaamm <mobongjubola@gmail.com>
2026-08-18 22:47:57 +0100

    feat: Add custom form for Riders admin with password handling

--- Foodcourt/admin.py
+++ Foodcourt/admin.py
@@ -1,9 +1,35 @@
 from django.contrib import admin
+from django import forms
 from .models import Riders
+
+
+class RidersAdminForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text='Raw password will be hashed on save. Leave blank when editing to keep the current password.',
+        required=False,
+    )
+
+    class Meta:
+        model = Riders
+        exclude = ('password',)
+
+    def save(self, commit=True):
+        rider = super().save(commit=False)
+        raw_password = self.cleaned_data.get('password')
+        if raw_password:
+            rider.set_password(raw_password)
+        elif not rider.pk:
+            rider.set_password('changeme123')
+        if commit:
+            rider.save()
+        return rider
 
 
 @admin.register(Riders)
 class RidersAdmin(admin.ModelAdmin):
+    form = RidersAdminForm
     list_display = ('username', 'email', 'phone', 'status', 'is_active', 'location', 'created_at')
     list_filter = ('status', 'is_active', 'vehicle_type', 'country')
     search_fields = ('username', 'email', 'phone', 'first_name', 'last_name', 'vehicle_plate', 'account_number', 'location')
